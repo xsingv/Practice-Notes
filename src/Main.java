4713274a88cb12fae5e41a25fb60508f698c36bb
xsing <2672946694@qq.com>
2017-06-05 17:41:44 +0800

    clear

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World! this is my win");
-        System.out.println("Hello World! this is my mac");
-    }
-}
